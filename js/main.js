075ca92e62eb9b592c668c59d1fcc06e0751ea56
Eric Leo <120332136+ELeo10@users.noreply.github.com>
2023-04-07 17:24:24 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.introFoto', {duration : 3000, origin: 'right'})
+sr.reveal('.introducao', {duration : 3000, origin: 'left'})
+sr.reveal('.bioFoto', {duration : 3000,  origin: 'left'})
+sr.reveal('.sobre-mim', {duration : 3000, origin:'right'})
+sr.reveal('.projetos-itens', {duration : 3000, origin:'bottom'})
+sr.reveal('.titulo-projeto', {duration : 3000, origin:'bottom'})
+sr.reveal('.github', {duration : 3000, origin:'bottom'})
+sr.reveal('.titulo-hab', {duration : 3000, origin:'bottom'})
+sr.reveal('.disc-conteiner', {duration : 3000, origin:'bottom'})
